a45e70e79fd5a1c80c779a6a82871ee2434f4a2c
Bkirk17 <bkirk@uwyo.edu>
2024-09-25 01:30:54 +0000

    done

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -21,40 +21,51 @@ print("Part One-----------------------------------------------------------------
 
 
 #print the entire list
+States = ["Wyoming","Colorado","Montana"]
 
 
 #now print the first element in the list
-
+print(States[0])
 
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
-
+print(states[-1])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
-
+print(f"{States[1]} is south of {States[0]}")
 
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
-
+States = ["Wyoming","Colorado","Montana"]
+States.Append["Washington"]
+States.Append["Oregon"]
+States.Append["Califorinia"]
+print(states)
 
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
-
-
+states[-2]= "Maine"
+print(states)
 #Insert the state Texas to be the third element in the list, again printing your list
-
+states[-1]= "Texas"
+print(states)
 
 #Using the `del` statement remove the fourth item from the list, print your list 
-
-
+del states[3]
+print(states)
 #Remove Texas using its value, print the list
-
+del states[4]
+print(states)
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
-
+print("unsorted",states)
+sorted_states=sorted(states)
+print("sorted",sorted_states)
 
 #Permanently sort your list in reverse order, printing it out
-
+state_sort(reverse=true)
+print("rsorted in reverse order", states)
 
 #Using the reverse method reverse the list and print it
-
+states.reverse()
+print("Reversed list:", states)
